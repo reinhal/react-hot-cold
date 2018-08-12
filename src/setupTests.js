b99bece03851d302e961b708126f279c9e999b19
Lisa Reinhardt <reinal@gmail.com>
2018-08-12 09:35:09 -0500

    added .travis.yml file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
-import {configure} from 'enzyme';
+// import {configure} from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// configure({adapter: new Adapter()});
+
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({adapter: new Adapter()});
+Enzyme.configure({adapter: new Adapter()});
